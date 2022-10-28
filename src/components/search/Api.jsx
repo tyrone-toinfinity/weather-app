27b816164533b154f6516cf2bc532f77a417f44c
Tyrone Woody <bobbywoody7@gmail.com>
2022-10-28 12:51:28 -0600

    hide keys

--- src/components/search/Api.jsx
+++ src/components/search/Api.jsx
@@ -1,7 +1,7 @@
 export const getApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "5fb2781583mshab1930424b4d584p1aab93jsnf987bbfc7d16",
+    "X-RapidAPI-Key": import.meta.env.VITE_WEATHER_API_KEY,
     "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -9,4 +9,4 @@ export const getApiOptions = {
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
-export const WEATHER_API_HIDDEN = "8d81f139947b5e3b458fed04579a3be5";
+export const WEATHER_API_HIDDEN = import.meta.env.VITE_WEATHER_API_KEY2;
